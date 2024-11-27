5a557a4926cac4327c36291d629c674ba7a38f92
Frank0606 <frankdanielsanchez55@gmail.com>
2024-11-26 18:26:36 -0600

    Cambios a paquetes

--- CapaServicios/src/java/dominio/ImpPaquetes.java
+++ CapaServicios/src/java/dominio/ImpPaquetes.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import mybatis.MybatisUtil;
 import org.apache.ibatis.session.SqlSession;
+import pojo.Mensaje;
 import pojo.Paquete;
 
 public class ImpPaquetes {
@@ -34,5 +35,111 @@ public class ImpPaquetes {
             paquete.setDescripcion("Error al conectarse a la base de datos.");
         }
         return respuesta;
+    }
+
+    public static List<Paquete> obtenerPaqueteId(Integer idPaquete) {
+        List<Paquete> respuesta = new ArrayList();
+        SqlSession conexionBD = MybatisUtil.obtenerConexion();
+        if (conexionBD != null) {
+            try {
+                List<Paquete> listaPaquetes = conexionBD.selectOne("paquete.obtenerPaqueteId", idPaquete);
+                if (listaPaquetes != null) {
+                    for (Paquete paquete : listaPaquetes) {
+                        respuesta.add(paquete);
+                    }
+                } else {
+                    Paquete paquete = new Paquete();
+                    paquete.setIdPaquete(-1);
+                    paquete.setDescripcion("No hay paquetes.");
+                }
+            } catch (Exception e) {
+                Paquete paquete = new Paquete();
+                paquete.setIdPaquete(-1);
+                paquete.setDescripcion(e.getMessage());
+            }
+        } else {
+            Paquete paquete = new Paquete();
+            paquete.setIdPaquete(-1);
+            paquete.setDescripcion("Error al conectarse a la base de datos.");
+        }
+        return respuesta;
+    }
+    
+    public static Mensaje registrarPaquete(Paquete paquete) {
+        Mensaje msj = new Mensaje();
+        SqlSession conexionbd = MybatisUtil.obtenerConexion();
+        if (conexionbd != null) {
+            try {
+                int resultado = conexionbd.insert("paquete.agregarPaquete", paquete);
+                conexionbd.commit();
+                if (resultado > 0) {
+                    msj.setError(false);
+                    msj.setMensaje("Se agrego un paquete exitosamente.");
+                } else {
+                    msj.setError(true);
+                    msj.setMensaje("No se pudo agregar el paquete. Intentalo de nuevo.");
+                }
+            } catch (Exception e) {
+                msj.setError(true);
+                msj.setMensaje(e.getMessage());
+            }
+        } else {
+            msj.setError(true);
+            msj.setMensaje("Por el momento no esta disponible el servicio. Intentelo mas tarde.");
+        }
+
+        return msj;
+    }
+    
+    public static Mensaje actualizarPaquete(Paquete paquete) {
+        Mensaje msj = new Mensaje();
+        SqlSession conexionbd = MybatisUtil.obtenerConexion();
+        if (conexionbd != null) {
+            try {
+                int resultado = conexionbd.update("paquete.actualizarPaquete", paquete);
+                conexionbd.commit();
+                if (resultado > 0) {
+                    msj.setError(false);
+                    msj.setMensaje("Se actualizo el paquete correctamente.");
+                } else {
+                    msj.setError(true);
+                    msj.setMensaje("No se pudo aztualizar el paquete. Intentalo de nuevo.");
+                }
+            } catch (Exception e) {
+                msj.setError(true);
+                msj.setMensaje(e.getMessage());
+            }
+        } else {
+            msj.setError(true);
+            msj.setMensaje("Por el momento no esta disponible el servicio. Intentelo mas tarde.");
+        }
+
+        return msj;
+    }
+    
+    public static Mensaje eliminarPaquete(Integer idPaquete) {
+        Mensaje msj = new Mensaje();
+        SqlSession conexionbd = MybatisUtil.obtenerConexion();
+        if (conexionbd != null) {
+            try {
+                int resultado = conexionbd.delete("paquete.eliminarPaquete", idPaquete);
+                conexionbd.commit();
+                if (resultado > 0) {
+                    msj.setError(false);
+                    msj.setMensaje("Se elimino al paquete correctamente.");
+                } else {
+                    msj.setError(true);
+                    msj.setMensaje("No se pudo eliminar al paquete. Intentalo de nuevo.");
+                }
+            } catch (Exception e) {
+                msj.setError(true);
+                msj.setMensaje(e.getMessage());
+            }
+        } else {
+            msj.setError(true);
+            msj.setMensaje("Por el momento no esta disponible el servicio. Intentelo mas tarde.");
+        }
+
+        return msj;
     }
 }
--- CapaServicios/src/java/ws/WSPaquete.java
+++ CapaServicios/src/java/ws/WSPaquete.java
@@ -1,13 +1,21 @@
 package ws;
 
+import com.google.gson.Gson;
 import dominio.ImpPaquetes;
 import java.util.List;
+import javax.ws.rs.BadRequestException;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriInfo;
+import pojo.Mensaje;
 import pojo.Paquete;
 
 @Path("paquete")
@@ -25,5 +33,60 @@ public class WSPaquete {
     @Produces(MediaType.APPLICATION_JSON)
     public List<Paquete> obtenerPaquetes() {
         return ImpPaquetes.obtenerPaquetes();
+    }
+    
+    @Path("id-paquete/{idPaquete}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Paquete> obtenerPaqueteId(@PathParam("idPaquete") Integer idPaquete) {
+        if (idPaquete != null && idPaquete > 0) {
+            return ImpPaquetes.obtenerPaqueteId(idPaquete);
+        }
+
+        throw new BadRequestException();
+    }
+    
+    @Path("agregar")
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Mensaje agregarPaquete(String jsonPaquete) {
+        try {
+            Gson gson = new Gson();
+            Paquete paquete = gson.fromJson(jsonPaquete, Paquete.class);
+            return ImpPaquetes.registrarPaquete(paquete);
+        } catch (Exception e) {
+            throw new BadRequestException();
+        }
+    }
+    
+    @Path("actualizar")
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Mensaje editarPaquete(String jsonPaquete) {
+        try {
+            Gson gson = new Gson();
+            Paquete paquete = gson.fromJson(jsonPaquete, Paquete.class);
+            return ImpPaquetes.actualizarPaquete(paquete);
+        } catch (Exception e) {
+            throw new BadRequestException();
+        }
+    }
+    
+    @Path("eliminar")
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Mensaje eliminarPaquete(String jsonIdPaquete) {
+        try {
+            Gson gson = new Gson();
+            System.out.println(jsonIdPaquete);
+            Paquete paquete = gson.fromJson(jsonIdPaquete, Paquete.class);
+            System.out.println(paquete);
+            return ImpPaquetes.eliminarPaquete(paquete.getIdPaquete());
+        } catch (Exception e) {
+            throw new BadRequestException();
+        }
     }
 }
