5fb8b57a4200ed5c9ace29b8f61e616095e84a80
Antonio Espinoza <retze2009@gmail.com>
2024-12-09 17:56:07 -0600

    Se agregaron las constantes, modelo y pojo.

---
+++ ClienteEscritorio/src/clienteescritorio/Utilidades/Constantes.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clienteescritorio.Utilidades;
+
+/**
+ *
+ * @author Manzano
+ */
+public class Constantes {
+    
+    public static final Integer ERROR_URL = 1001;
+    public static final Integer ERROR_PETICION = 1002;
+    public static final String URL_WS = "http://localhost:8084/TimeFast/api/";
+}
